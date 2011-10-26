7396e019b818b6df64a9fc1bf0b056705eb8dc25
Ashish Agarwal <agarwal1975@gmail.com>
2011-10-26 16:05:55 -0400

    Deleted lots of things and switched to using Core instead of Batteries. Basically a fresh start.

--- src/lib/hitscore_std.ml
+++ src/lib/hitscore_std.ml
@@ -1,42 +1,4 @@
-
-
-
-include BatPervasives
-include BatPrintf
-
-module List = struct 
-  include List
-  include ListLabels
-  include BatList
-  include BatList.Labels
-
-  let assoc_exn = assoc
-  let hd_exn = hd
-  let tl_exn = tl
-
-  include BatList.Exceptionless
-  include BatList.Labels.LExceptionless
-
-end
-
-module String = struct
-  include StringLabels
-  include BatString
-  include BatString.Exceptionless
-  let split_exn a b = BatString.split a b
-end
-
-module Array = struct
-  include Array 
-  include BatArray 
-  include BatArray.Labels
-end
-
-module Enum = struct
-  include BatEnum
-  include BatEnum.Labels
-  include BatEnum.Labels.LExceptionless
-end
+include Core.Std
 
 module Util = struct
 
@@ -46,11 +8,9 @@ module Util = struct
   let file_last_modified_time file =
     let open Unix in
     let open CalendarLib in
-    ((stat file).st_mtime |> localtime |> Calendar.from_unixtm),
+    ((stat file).st_mtime |! localtime |! Calendar.from_unixtm),
     Time_Zone.Local
 end
 
-module Map = BatMap
-module IntMap = BatMap.IntMap
-module Set = BatSet
-module StringSet = BatSet.StringSet
+module IntMap = Map.Make(Int)
+module StringSet = Set.Make(String)
