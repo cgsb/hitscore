a9964adb770f5a175f9d8d5a74cc8b5964d98777
Ashish Agarwal <agarwal1975@gmail.com>
2011-10-26 16:05:55 -0400

    Deleted lots of things and switched to using Core instead of Batteries. Basically a fresh start.

--- src/lib/hitscore.ml
+++ src/lib/hitscore.ml
@@ -1,7 +1,1 @@
-
-module BclToFastq = Hitscore_bclToFastq
 module Conf       = Hitscore_conf
-module Flowcell   = Hitscore_flowcell
-module Library    = Hitscore_library
-module Sample     = Hitscore_sample
-
