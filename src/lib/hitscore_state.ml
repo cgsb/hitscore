143bd39e5791fced924fa3859cf33609dbfb97b2
Ashish Agarwal <agarwal1975@gmail.com>
2011-10-28 16:06:22 -0400

    Added first action, AddPerson, and method to update State with this action.

--- src/lib/hitscore_state.ml
+++ src/lib/hitscore_state.ml
@@ -1,6 +1,9 @@
-open Hitscore_std
+module Result = Core.Std.Result
+open Result
+type dbh = (string, bool) Hashtbl.t PGOCaml.t
 
 module Config = Hitscore_config
+module Person = Hitscore_person
 
 type t = {
   config : Config.t;
@@ -20,3 +23,18 @@ let get x =
     in
     Ok {config=x; dbh}
   with e -> Error e
+
+
+type action =
+    | AddPerson of Person.t
+
+let update_exn t = function
+  | AddPerson x ->
+      let open Person in
+      let f = x.first in
+      let m = match x.middle_initial with Some m -> Some (String.make 1 m) | None -> None in
+      let l = x.last in
+      let e = x.email in
+      PGSQL(t.dbh)
+        "INSERT INTO person (first_name,middle_initial,last_name,email)
+         VALUES ($f, $?m, $l, $e)"
