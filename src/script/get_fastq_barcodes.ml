354cc8ab7575aca3763ae6dc236d2d786d4ebdee
Sebastien Mondet <seb@mondet.org>
2011-11-15 11:05:27 -0500

    Make get_fastq_barcodes.ml use the read 2

--- src/script/get_fastq_barcodes.ml
+++ src/script/get_fastq_barcodes.ml
@@ -23,13 +23,11 @@ let () =
     let rec parse fastq =
       match Enum.get fastq with
       | Some (title, sequence, another_title, quality_score) ->
-        let { Sequme_illumina.Fastq.index; _ } =
-          Sequme_illumina.Fastq.sequence_id_of_string title in
-        begin match HT.find results index with
+        begin match HT.find results sequence with
         | Some count ->
-          HT.replace results index (count + 1)
+          HT.replace results sequence (count + 1)
         | None -> 
-          HT.add results index 1
+          HT.add results sequence 1
         end;
         parse fastq
       | None -> ()
@@ -40,17 +38,17 @@ let () =
   
   let ns = HT.create 6 in
   List.iter (results |> HT.enum |> List.of_enum |> List.fast_sort)
-    ~f:(fun (index, count) ->
+    ~f:(fun (sequence, count) ->
       let number_of_Ns = 
         String.fold_left 
-          (fun x c -> x + (if c = 'N' then 1 else 0)) 0 index in
+          (fun x c -> x + (if c = 'N' then 1 else 0)) 0 sequence in
       begin match HT.find ns number_of_Ns with
       | Some c ->
         HT.replace ns number_of_Ns (c + count)
       | None -> 
         HT.add ns number_of_Ns count
       end;
-      printf "Index %-20s : %d\n%!" index count;
+      printf "Index %-20s : %d\n%!" sequence count;
     );
   List.iter (ns |> HT.enum |> List.of_enum |> List.fast_sort)
     ~f:(fun (nbs, count) ->
