5c2d1a67d012c49313b62cf6d1b1375cd82c39b5
Sebastien Mondet <seb@mondet.org>
2011-12-20 18:29:20 -0500

    Implement first working version of the migrator

--- src/migrator/migrator.ml
+++ src/migrator/migrator.ml
@@ -13,9 +13,152 @@ let v011_to_v02 file_in file_out =
 
   printf "s011 version: %s\n" s011.V011.version;
 
+  let check_barcode_provider bc =
+    V02.Enumeration_barcode_provider.of_string bc |> 
+        Result.ok_exn ~fail:(Failure "barcode provider is wrong") |>
+            V02.Enumeration_barcode_provider.to_string
+  in
+
+
+  let record_lane, record_input_library, record_stock_library =
+    let sl_rl1_rl2 = ref [] in
+    let stocks =
+      List.map s011.V011.record_stock_library 
+        (fun cache ->
+          let (g_id, g_created, g_last_modified, name, sample,
+               protocol, application, stranded, truseq_control,
+               rnaseq_control, read_length_1, read_length_2, barcode_type,
+               barcodes, note) = cache in
+   
+          sl_rl1_rl2 := (g_id, read_length_1, read_length_2) :: !sl_rl1_rl2;
+
+          let project = None in
+          let custom_barcodes = [| |] in
+          let p5_adapter_length = Some 58l in
+          let p7_adapter_length = Some 59l in
+          let preparator = None in
+          (g_id, g_created, g_last_modified, 
+           name, project, sample,
+           protocol, application, stranded, truseq_control,
+           rnaseq_control,
+           check_barcode_provider barcode_type, 
+           barcodes, custom_barcodes,
+           p5_adapter_length, p7_adapter_length, preparator, note)
+        )
+    in
+    let il_sl_contacts = ref [] in
+    let inputs =
+      List.map s011.V011.record_input_library (fun cache ->
+        let (g_id, g_created, g_last_modified, library,
+             submission_date, contacts, volume_uL, concentration_nM,
+             user_db, note) = cache in
+
+        il_sl_contacts := (g_id, library, contacts) :: !il_sl_contacts;
+
+        (g_id, g_created, g_last_modified, library, submission_date,
+         volume_uL, concentration_nM, user_db, note)) in
+
+    let lanes =
+      List.map s011.V011.record_lane (fun cache ->
+        let (g_id, g_created, g_last_modified, seeding_concentration,
+             total_volume, libraries, pooled_percentages) = cache in
+
+        let seeding_concentration_pM = seeding_concentration in
+
+        let contacts =
+          let redundant_contacts =
+            List.find_all !il_sl_contacts ~f:(fun (il, sl, cs) ->
+              List.exists (Array.to_list libraries) ~f:((=) il))
+          in
+          let check_uniformity x =
+            List.reduce x ~f:(fun (il1, sl1, ca1) (il2, sl2, ca2) ->
+              if il1 = 149l (* PhiX_v2 *) ||
+                il1 = 150l (* PhiX_v3 *) ||
+                ca1 = ca2 then (il1, sl1, ca1)
+              else (
+                List.iter redundant_contacts (fun (il, sl, ca) -> 
+                  printf "[%ld] " il;
+                  Array.iter ca (fun c ->
+                    printf "%ld, " c);
+                  printf "\n"); 
+                failwith "Contact list is not uniform !!")) in
+          match check_uniformity redundant_contacts with
+          | Some  (il, sl, ca) -> ca
+          | None -> failwith "a lane with no libraries???"
+        in
+
+        let requested_read_length_1, requested_read_length_2 =
+          let stock_libs = 
+            List.find_all !il_sl_contacts ~f:(fun (il, sl, cs) ->
+              List.exists (Array.to_list libraries) ~f:((=) il)) |>
+                List.map ~f:(fun (il, sl, ca) -> sl) |>
+                    List.dedup in
+          (* printf "SLs: "; *)
+          (* List.iter stock_libs (printf "%ld, "); *)
+          let read_lengths = 
+            List.find_all !sl_rl1_rl2 ~f:(fun (sl, rl1, rl2) ->
+              sl <> 149l (* PhiX_v2 *) 
+              && sl <> 150l (* PhiX_v3 *)
+            && List.exists stock_libs ~f:((=) sl)) |>
+                List.map ~f:(fun (sl, rl1, rl2) -> (rl1, rl2)) |>
+                    List.dedup
+          in
+          match read_lengths with
+          | [] -> (* Should be a PhiX *)
+            let flowcell_lanes =
+              List.find_map s011.V011.record_flowcell ~f:(fun cache ->
+                let (_, g_created, g_last_modified, serial_name, lanes) = cache in
+                if Array.mem g_id lanes then Some lanes else None) |>
+                  function
+                  | Some s -> Array.to_list s
+                  | None -> failwith "Can't find in which flowcell the lane is"
+            in
+            let sister_libs =
+              List.find_map flowcell_lanes (fun l ->
+                List.find_map s011.V011.record_lane ~f:(fun cache ->
+                  let (id, g_created, g_last_modified, seeding_concentration,
+                       total_volume, libraries, pooled_percentages) = cache in
+                  if id = l && g_id <> id then
+                    Some (Array.to_list libraries) else None)) |>
+                  function
+                  | Some s -> s
+                  | None -> failwith "Cannot find any other library ??"
+            in
+            let sister_lib_read_length =
+              (* We assume input_lib.id = stock_lib.id *)
+              List.find_map s011.V011.record_stock_library ~f:(fun cache ->
+                let (id, g_created, g_last_modified, name, sample,
+                     protocol, application, stranded, truseq_control,
+                     rnaseq_control, read_length_1, read_length_2, barcode_type,
+                     barcodes, note) = cache in
+                if id <> 149l && id <> 150l &&
+                  List.exists sister_libs ((=) id)
+                then Some (read_length_1, read_length_2)
+                  else None) |>
+                  function Some s -> s 
+                  | None -> failwith "can't find sister read length"
+            in
+            sister_lib_read_length
+          | [ one_rl1, Some one_rl2 ] -> (one_rl1, Some one_rl2)
+          | l ->
+            printf "Don't know what to do:\n";
+            List.iteri l ~f:(fun i (rl1, rl2) ->
+              printf "(%d: %ld ...) " i rl1);
+            printf "\n";
+            failwith "wrong requested_read_lengths"
+
+        in
+
+
+        (g_id, g_created, g_last_modified, seeding_concentration_pM,
+        total_volume, libraries, pooled_percentages,
+        requested_read_length_1, requested_read_length_2, contacts))
+    in
+
+    lanes, inputs, stocks in
 
   let d02 = {
-    V02.version = "0.2";
+    V02.version = "0.1-dev";
     file_system = s011.V011.file_system;
     record_log = s011.V011.record_log;
     record_person = s011.V011.record_person;
@@ -23,29 +166,29 @@ let v011_to_v02 file_in file_out =
     record_sample = s011.V011.record_sample;
     record_protocol = s011.V011.record_protocol;
     record_custom_barcode = [];
-    record_stock_library = (* TODO s011.V011.record_stock_library *) [];
+    record_stock_library;
     record_key_value = s011.V011.record_key_value;
-    record_input_library = (* s011.V011.record_input_library *) [];
-    record_lane = (* s011.V011.record_lane *) [];
+    record_input_library;
+    record_lane;
     record_flowcell = s011.V011.record_flowcell;
     record_invoicing = [];
-    record_bioanalyzer = [];
-    record_agarose_gel = [];
-    record_hiseq_raw = [];
-    record_inaccessible_hiseq_raw = [];
-    record_sample_sheet = [];
-    function_assemble_sample_sheet = [];
+    record_bioanalyzer = s011.V011.record_bioanalyzer;
+    record_agarose_gel = s011.V011.record_agarose_gel;
+    record_hiseq_raw = s011.V011.record_hiseq_raw;
+    record_inaccessible_hiseq_raw = s011.V011.record_inaccessible_hiseq_raw;
+    record_sample_sheet = s011.V011.record_sample_sheet;
+    function_assemble_sample_sheet = s011.V011.function_assemble_sample_sheet;
 
-    record_bcl_to_fastq_unaligned = [];
-    function_bcl_to_fastq = [];
-    function_transfer_hisqeq_raw = [];
+    record_bcl_to_fastq_unaligned = s011.V011.record_bcl_to_fastq_unaligned;
+    function_bcl_to_fastq = s011.V011.function_bcl_to_fastq;
+    function_transfer_hisqeq_raw = s011.V011.function_transfer_hisqeq_raw;
 
-    function_delete_intensities = [];
+    function_delete_intensities = s011.V011.function_delete_intensities;
 
-    record_hiseq_checksum = [];
-    function_dircmp_raw = [];
-    record_client_fastqs_dir = [];
-    function_prepare_delivery = [];
+    record_hiseq_checksum = s011.V011.record_hiseq_checksum;
+    function_dircmp_raw = s011.V011.function_dircmp_raw;
+    record_client_fastqs_dir = s011.V011.record_client_fastqs_dir;
+    function_prepare_delivery = s011.V011.function_prepare_delivery;
   } in
 
   Out_channel.(with_file file_out ~f:(fun o ->
