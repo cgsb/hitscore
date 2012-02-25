30ee949bfbcf22ef5820b181431362a896586733
Sebastien Mondet <seb@mondet.org>
2012-02-24 19:34:49 -0500

    Create Common.check_hiseq_raw_availability

--- src/lib/hitscore_common.ml
+++ src/lib/hitscore_common.ml
@@ -18,6 +18,20 @@ module type COMMON = sig
     with module Result_IO = Result_IO
     with module Layout = Layout
 
+  val check_hiseq_raw_availability :
+    dbh:Layout.db_handle ->
+    hiseq_raw:Layout.Record_hiseq_raw.pointer ->
+    (Layout.Record_inaccessible_hiseq_raw.pointer *
+       Layout.Record_hiseq_raw.pointer Hitscore_std.List.t,
+     [> `hiseq_dir_deleted
+     | `layout_inconsistency of
+         [> `record_inaccessible_hiseq_raw ] *
+           [> `insert_did_not_return_one_id of string * int32 list
+           | `no_last_modified_timestamp of
+               Layout.Record_inaccessible_hiseq_raw.pointer
+           | `select_did_not_return_one_tuple of string * int ]
+     | `pg_exn of exn ])
+      Result_IO.monad
 end
 
 
@@ -37,15 +51,44 @@ module Make
     with module Result_IO = Result_IO
     with module Layout = Layout
     with module Access_rights = Access_rights
-     = struct
-    module Configuration = Configuration
-    module Result_IO = Result_IO
-    module Access_rights = Access_rights
-    module Layout = Layout
+= struct
+  module Configuration = Configuration
+  module Result_IO = Result_IO
+  module Access_rights = Access_rights
+  module Layout = Layout
 
-    open Hitscore_std
-    open Result_IO
+  open Hitscore_std
+  open Result_IO
 
+  let check_hiseq_raw_availability ~dbh ~hiseq_raw =
+    Layout.Record_inaccessible_hiseq_raw.(
+      get_all ~dbh >>= fun all ->
+      of_list_sequential all ~f:(fun p ->
+        get ~dbh p >>= fun {g_last_modified; deleted; _} ->
+        begin match g_last_modified with
+        | Some t -> return (p, t, Array.to_list deleted)
+        | None -> error (`layout_inconsistency 
+                            (`record_inaccessible_hiseq_raw,
+                             `no_last_modified_timestamp p))
+        end)
+      >>| List.sort ~cmp:(fun a b -> compare (snd3 b) (snd3 a))
+      >>=
+        (function
+        | [] ->
+          printf "There were no inaccessible_hiseq_raw => \
+                       creating the empty one.\n";
+          Layout.Record_inaccessible_hiseq_raw.add_value ~dbh ~deleted:[| |]
+          >>= fun pointer ->
+          return (pointer, [])
+        | (h, ts, l) :: t as whole-> 
+          printf "Last inaccessible_hiseq_raw: %ld on %s\n"
+            h.Layout.Record_inaccessible_hiseq_raw.id
+            (Time.to_string ts);
+          return (h, List.map whole trd3 |! List.flatten))
+      >>= fun (last_avail, all_deleted) ->
+      if List.exists all_deleted ((=) hiseq_raw) then
+        error `hiseq_dir_deleted
+      else
+        return (last_avail, all_deleted))
 
-
-    end
+end
--- src/lib/hitscore_delete_intensities.ml
+++ src/lib/hitscore_delete_intensities.ml
@@ -15,60 +15,32 @@ module Make
   open Result_IO
 
   let register ~dbh ~hiseq_raw =
-    Layout.Record_inaccessible_hiseq_raw.(
-      get_all ~dbh >>= fun all ->
-      of_list_sequential all ~f:(fun p ->
-        get ~dbh p >>= fun {g_last_modified; deleted; _} ->
-        begin match g_last_modified with
-        | Some t -> return (p, t, Array.to_list deleted)
-        | None -> error (`layout_inconsistency 
-                            (`record_inaccessible_hiseq_raw,
-                             `no_last_modified_timestamp p))
-        end)
-      >>| List.sort ~cmp:(fun a b -> compare (snd3 b) (snd3 a))
-      >>=
-        (function
-        | [] ->
-          printf "There were no inaccessible_hiseq_raw => \
-                       creating the empty one.\n";
-          Layout.Record_inaccessible_hiseq_raw.add_value ~dbh ~deleted:[| |]
-          >>= fun pointer ->
-          return (pointer, [])
-        | (h, ts, l) :: t as whole-> 
-          printf "Last inaccessible_hiseq_raw: %ld on %s\n"
-            h.Layout.Record_inaccessible_hiseq_raw.id
-            (Time.to_string ts);
-          return (h, List.map whole trd3 |! List.flatten))
-      >>= fun (last_avail, all_deleted) ->
-      if List.exists all_deleted ((=) hiseq_raw) then
-        error `hiseq_dir_deleted
-      else
-        return (last_avail, all_deleted)
-         >>= fun (availability, all_deleted) ->
-      Layout.Record_hiseq_raw.(
-        get ~dbh hiseq_raw
-        >>= fun hiseq_raw_info ->
-        Layout.Function_delete_intensities.(
-          add_evaluation ~dbh
-            ~recomputable:false ~recompute_penalty:0.
-            ~hiseq_raw ~availability >>= set_started ~dbh
-          >>= fun evaluation ->
-          let {flowcell_name; read_length_1; read_length_2; read_length_index;
-               with_intensities; run_date; host; hiseq_dir_name;} =
-            hiseq_raw_info in
-          add_value ~dbh
-            ~flowcell_name ~read_length_1 ?read_length_2 ?read_length_index
-            ~with_intensities:false ~run_date ~host ~hiseq_dir_name
-          >>= fun new_hiseq_raw ->
-          Layout.Record_inaccessible_hiseq_raw.add_value
-            ~dbh ~deleted:(Array.of_list (hiseq_raw :: (List.dedup all_deleted)))
-          >>= fun (_: Layout.Record_inaccessible_hiseq_raw.pointer) ->
-          set_succeeded ~dbh evaluation ~result:new_hiseq_raw
-          >>= fun successful_evaluation ->
-          Layout.Record_log.add_value ~dbh
-            ~log:(sprintf "(delete_intensities success %ld)"
-                    successful_evaluation.id)
-          >>= fun (_: Layout.Record_log.pointer) ->
-          return successful_evaluation)))
+    check_hiseq_raw_availability ~dbh ~hiseq_raw
+    >>= fun (availability, all_deleted) ->
+    Layout.Record_hiseq_raw.(
+      get ~dbh hiseq_raw
+      >>= fun hiseq_raw_info ->
+      Layout.Function_delete_intensities.(
+        add_evaluation ~dbh
+          ~recomputable:false ~recompute_penalty:0.
+          ~hiseq_raw ~availability >>= set_started ~dbh
+        >>= fun evaluation ->
+        let {flowcell_name; read_length_1; read_length_2; read_length_index;
+             with_intensities; run_date; host; hiseq_dir_name;} =
+          hiseq_raw_info in
+        add_value ~dbh
+          ~flowcell_name ~read_length_1 ?read_length_2 ?read_length_index
+          ~with_intensities:false ~run_date ~host ~hiseq_dir_name
+        >>= fun new_hiseq_raw ->
+        Layout.Record_inaccessible_hiseq_raw.add_value
+          ~dbh ~deleted:(Array.of_list (hiseq_raw :: (List.dedup all_deleted)))
+        >>= fun (_: Layout.Record_inaccessible_hiseq_raw.pointer) ->
+        set_succeeded ~dbh evaluation ~result:new_hiseq_raw
+        >>= fun successful_evaluation ->
+        Layout.Record_log.add_value ~dbh
+          ~log:(sprintf "(delete_intensities success %ld)"
+                  successful_evaluation.id)
+        >>= fun (_: Layout.Record_log.pointer) ->
+        return successful_evaluation))
                         
   end
