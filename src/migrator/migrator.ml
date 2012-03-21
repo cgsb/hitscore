108a137f3458bdfb6a099572616552a767f9b5e9
Sebastien Mondet <seb@mondet.org>
2012-03-21 11:24:25 -0400

    Update the migrator to the new FS representation

--- src/migrator/migrator.ml
+++ src/migrator/migrator.ml
@@ -53,8 +53,10 @@ let v06_to_v07 file_in file_out =
   let dump_v06 =
     In_channel.(with_file file_in ~f:input_all) |! Sexplib.Sexp.of_string in
 
+  let module V06M = V06.Make (Result_IO) in
+  let module V07M = V07.Make (Result_IO) in
+
   let () =
-    let module V06M = V06.Make (Result_IO) in
     V06M.dump_of_sexp dump_v06 |! ignore in
 
   let dump_v07 =
@@ -64,7 +66,35 @@ let v06_to_v07 file_in file_out =
       | Atom a -> Atom a
       | List [Atom "version"; Atom old_version]
           when old_version = V06.Info.version ->
-        List [Atom "version"; Atom V07.Info.version ] 
+        List [Atom "version"; Atom "0.7-dev" ] 
+        (* List [Atom "version"; Atom V07.Info.version ]  *)
+
+      | List [Atom "file_system"; List volume_sexps] ->
+        let module FS06 = V06M.File_system in
+        let module FS07 = V07M.File_system in
+        printf "File system: %d volumes\n" (List.length volume_sexps);
+        let fail = Failure "transforming volumes" in
+        let v06_volumes =
+          List.map volume_sexps FS06.volume_of_sexp in
+        let v07_volumes =
+          List.map v06_volumes (fun vol06 ->
+            let {FS06.volume_entry; files} = vol06 in
+            let {FS06.v_id; v_toplevel; v_hr_tag; v_content } = volume_entry in
+            let trees06 = FS06.volume_trees vol06 |! Result.ok_exn ~fail in
+            let trees07 =
+              let rec f = function
+                | FS06.File (s, t)          -> FS07.File (s, t)
+                | FS06.Directory (s, t, tl) -> FS07.Directory (s, t, List.map ~f tl) 
+                | FS06.Opaque (s, t)        -> FS07.Opaque (s, t) in
+              List.map trees06 f in
+            FS07.({
+              volume_pointer = { id = v_id };
+              volume_kind = kind_of_toplevel v_toplevel |! Result.ok_exn ~fail;
+              volume_content = Tree (v_hr_tag, trees07);
+            })) in
+        let v07_sexps = List.map v07_volumes FS07.sexp_of_volume in
+        List [Atom "file_system"; List v07_sexps]
+
       | List l ->
         List (List.map ~f:parse l) in
     dump_v06
@@ -76,7 +106,6 @@ let v06_to_v07 file_in file_out =
   in
   
   let () =
-    let module V07M = V07.Make (Result_IO) in
     try
       V07M.dump_of_sexp dump_v07 |! ignore
     with e ->
