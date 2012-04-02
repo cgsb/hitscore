592f7e17552c3e76520b006f1db91a4239d185b1
Sebastien Mondet <seb@mondet.org>
2012-04-02 17:02:45 -0400

    Parse CA index and check completely client certs

--- src/server/hitscored.ml
+++ src/server/hitscored.ml
@@ -1,6 +1,62 @@
 open Hitscored_std
 
+module String_map = Map.Make(String)
 
+let read_file file =
+  wrap_io Lwt_io.(fun () -> with_file ~mode:input file (fun i -> read i)) ()
+
+let parse_ASN1_UTCTIME_format_exn s =
+  (* YYMMDDHHMMSSZ e.g. 220328212233Z *)
+  if String.length s  <> 13
+  then failwithf "invalid_ASN1_date %s" s ()
+  else
+    try
+      let get2 i = Int.of_string (String.sub s i 2) in
+      let y = 2000 + (get2 0) in
+      let m = get2 2 |! Month.of_int_exn in
+      let d = get2 4 in
+      let hr = get2 6 in
+      let mn = get2 8 in
+      let sec = get2 10 in
+      Time.(of_date_ofday
+              Zone.utc Date.(create ~m ~y ~d) Ofday.(create ~hr ~min:mn ~sec ()))
+    with
+    | e -> failwithf "invalid_ASN1_date %s" s ()
+      
+let common_name_of_subject subj =
+  List.find_map (String.split subj ~on:'/') (function
+  | s when String.prefix s 3 = "CN=" ->
+    begin match String.split ~on:'-' (String.drop_prefix s 3) with
+    | [ login; _ ] -> Some login
+    | _ -> None
+    end
+  | _ -> None)
+  
+(* http://old.nabble.com/Format-of-index.txt-file-td21557292.html *)
+let get_CA_index ca_cert =
+  let ca_path = Filename.dirname ca_cert in
+  read_file Filename.(concat ca_path "index.txt")
+  >>| String.split ~on:'\n'
+  >>| List.map ~f:(String.split ~on:'\t')
+  >>= fun lines ->
+  let date = parse_ASN1_UTCTIME_format_exn in
+  (try
+     List.fold_left lines ~init:String_map.empty ~f:(fun ca_index line ->
+       begin match line with
+       | "V" :: exp_date :: "" :: serial :: _ :: name :: [] ->
+         String_map.add ~key:name ~data:(`valid (date exp_date, serial)) ca_index
+       | "R" :: exp_date :: rev_date :: serial :: _ :: name :: [] ->
+         String_map.add ~key:name ~data:(`revoked (date rev_date, serial)) ca_index
+       | "E" :: exp_date :: _ :: serial :: _ :: name :: [] ->
+         String_map.add ~key:name
+           ~data:(`expired (date exp_date, serial)) ca_index
+       | [] | [""] -> ca_index
+       | l ->
+         failwithf "get_CA_index:cannot_parse [%s]" (String.concat ~sep:", " l) ()
+       end)
+      |! return
+   with 
+   | e -> error (`wrong_CA_index_format e))
 
     
 let main ?ca_cert ~cert_key () =
@@ -11,14 +67,26 @@ let main ?ca_cert ~cert_key () =
   >>= fun ssl_context ->
   Flow_net.server_socket ~port:2000 >>= fun socket ->
 
-  let check_client_certificate =
-    Option.map ca_cert (fun _ ->
-      fun c ->
-        dbg "check_client_certificate:\n  Issuer: %s\n  Subject: %s"
-          (Ssl.get_issuer c) (Ssl.get_subject c)
-      >>= fun () ->
-        return true)
-  in
+  of_option ca_cert (fun ca_cert ->
+    get_CA_index ca_cert
+    >>= fun ca_index ->
+    return (fun c ->
+      dbg "check_client_certificate:\n  Issuer: %s\n  Subject: %s"
+        (Ssl.get_issuer c) (Ssl.get_subject c) >>= fun () ->
+      let subj = (Ssl.get_subject c) in
+      let cn = common_name_of_subject subj in
+      begin match String_map.find ca_index subj with
+      | None -> return `certificate_not_found
+      | Some (`valid (time, serial)) when Time.(time >. now ()) ->
+        dbg "TIME: %s, CN: %s" Time.(to_string time)
+          Option.(value ~default:"DID NOT PARSE" cn) >>= fun () ->
+        return `ok
+      | Some (`valid (time, _)) | Some (`expired (time, _)) ->
+        return `expired
+      | Some (`revoked _) -> return `revoked
+      end))
+  >>= fun check_client_certificate ->
+
   Flow_net.ssl_accept_loop ?check_client_certificate ssl_context socket
     (fun client_socket client_kind ->
       begin match client_kind with
@@ -26,6 +94,10 @@ let main ?ca_cert ~cert_key () =
         dbg "The client has a wrong certificate"
       | `invalid_client (`expired_certificate _) ->
         dbg "The client has an expired certificate"
+      | `invalid_client (`revoked_certificate _) ->
+        dbg "The client has a revoked certificate"
+      | `invalid_client (`certificate_not_found _) ->
+        dbg "The client has a not-found certificate"
       | `anonymous_client
       | `valid_client _ ->
         let inchan = Lwt_ssl.in_channel_of_descr client_socket in
