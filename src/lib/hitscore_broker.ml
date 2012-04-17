45a9506e21d9952c18cec6cfece88028297947d6
Sebastien Mondet <seb@mondet.org>
2012-04-17 18:20:13 -0400

    Add function Broker.person_affairs

--- src/lib/hitscore_broker.ml
+++ src/lib/hitscore_broker.ml
@@ -1,3 +1,5 @@
+
+
 module type BROKER = sig
 
 
@@ -50,6 +52,22 @@ module type BROKER = sig
      | `pg_exn of exn ])
       Common.Flow.monad
       
+  type lane = {
+    lane_t: Layout.Record_lane.t;
+    lane_index: int; (* Lane index form 1 to 8 *)
+  }
+  type filtered_flowcell = {
+    ff_t: Layout.Record_flowcell.t;
+    ff_id: string;
+    ff_lanes: lane list;
+    ff_runs: Layout.Record_hiseq_run.t list;
+  }
+  type person_affairs = {
+    pa_flowcells: filtered_flowcell list;
+  }
+
+  val person_affairs: t -> person:Layout.Record_person.t -> person_affairs
+
 end
 
 module Make
@@ -105,4 +123,54 @@ module Make
       t.current_dump <- { t.current_dump with Layout.record_person };
       return ()
         
+
+    type lane = {
+      lane_t: Layout.Record_lane.t;
+      lane_index: int; (* Lane index form 1 to 8 *)
+    }
+    type filtered_flowcell = {
+      ff_t: Layout.Record_flowcell.t;
+      ff_id: string;
+      ff_lanes: lane list;
+      ff_runs: Layout.Record_hiseq_run.t list;
+    }
+    type person_affairs = {
+      pa_flowcells: filtered_flowcell list;
+    }
+
+    let person_affairs t ~person =
+      let module LP = Layout.Record_person in
+      let module LFC = Layout.Record_flowcell in
+      let module LL = Layout.Record_lane in
+      let flowcells =
+        List.filter_map t.current_dump.Layout.record_flowcell
+          (fun fc ->
+            let {LFC. g_id; serial_name; lanes} = fc in
+            let p_id = LP.unsafe_cast person.LP.g_id in
+            let lanes_of_flowcell =
+              List.filter_map t.current_dump.Layout.record_lane
+                (fun t ->
+                  let t_pointer = LL.unsafe_cast t.LL.g_id in
+                  match Array.findi lanes ~f:((=) t_pointer) with
+                  | Some array_index ->
+                    if Array.exists t.LL.contacts ~f:((=) p_id)
+                    then Some { lane_t = t; lane_index = array_index + 1}
+                    else None
+                  | None -> None)
+            in
+            match lanes_of_flowcell with
+            | [] -> None
+            | l ->
+              let ff_runs =
+                let open Layout.Record_hiseq_run in
+                let flowcell_pointer = LFC.unsafe_cast g_id in
+                List.filter t.current_dump.Layout.record_hiseq_run ~f:(fun hs ->
+                  hs.flowcell_a = Some flowcell_pointer
+                   || hs.flowcell_b = Some flowcell_pointer)
+              in
+              Some  {ff_t = fc; ff_id = serial_name; ff_lanes = l; ff_runs})
+      in
+      {pa_flowcells = flowcells}
+        
+
   end
