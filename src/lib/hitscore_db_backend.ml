d184d55f285de656f76a23d7ee9ac012d32ae8cc
Sebastien Mondet <seb@mondet.org>
2012-05-15 15:21:57 -0400

    Fix Sql_query.update_sequence (add ORDER BY)

--- src/lib/hitscore_db_backend.ml
+++ src/lib/hitscore_db_backend.ml
@@ -72,7 +72,7 @@ module Sql_query = struct
   let update_sequence : t =
     "select setval('id_sequence', \
      (select greatest (record.id, function.id, volume.id) \
-      from record,volume,function limit 1))"
+      from record,volume,function ORDER BY 1 DESC LIMIT 1))"
 
   let create_record: t =
     sprintf
