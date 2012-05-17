03a7d1fa18f558771816e8157343915fa18740f0
Sebastien Mondet <seb@mondet.org>
2012-05-17 15:20:02 -0400

    Add long-term solution for annoying DB input bugs
    The type 'text' literals were interpreted differently given the
    configuration and version of the PG server. I replaced everything with
    'bytea' a.k.a. binary strings. I also imported (and modified) code
    from PG'OCaml to encode and decode binary strings.

--- src/lib/hitscore_db_backend.ml
+++ src/lib/hitscore_db_backend.ml
@@ -4,9 +4,104 @@ open Hitscore_std
   
 module Timestamp = struct 
   include Core.Std.Time
-  let () = write_new_string_and_sexp_formats__read_both ()
+  let () = write_new_string_and_sexp_formats__read_only_new ()
 end
 
+module Bytea = struct
+
+  let is_first_oct_digit c = c >= '0' && c <= '3'
+  let is_oct_digit c = c >= '0' && c <= '7'
+  let oct_val c = Char.to_int c - 0x30
+
+  let is_hex_digit = function '0'..'9' | 'a'..'f' | 'A'..'F' -> true | _ -> false
+
+  let hex_val c =
+    let offset = match c with
+      | '0'..'9' -> 0x30
+      | 'a'..'f' -> 0x57
+      | 'A'..'F' -> 0x37
+      | _	       -> failwith "hex_val"
+    in Char.to_int c - offset
+
+  (* Deserialiser for the new 'hex' format introduced in PostgreSQL 9.0. *)
+  let bytea_of_string_hex str =
+    let len = String.length str in
+    let buf = Buffer.create ((len-2)/2) in
+    let i = ref 3 in
+    while !i < len do
+      let hi_nibble = str.[!i-1] in
+      let lo_nibble = str.[!i] in
+      i := !i+2;
+      if is_hex_digit hi_nibble && is_hex_digit lo_nibble
+      then begin
+        let byte = ((hex_val hi_nibble) lsl 4) + (hex_val lo_nibble) in
+        Buffer.add_char buf (Char.of_int_exn byte)
+      end
+    done;
+    Buffer.contents buf
+
+  (* Deserialiser for the old 'escape' format used in PostgreSQL < 9.0. *)
+  let bytea_of_string_escape str =
+    let len = String.length str in
+    let buf = Buffer.create len in
+    let i = ref 0 in
+    while !i < len do
+      let c = str.[!i] in
+      if c = '\\' then (
+        incr i;
+        if !i < len && str.[!i] = '\\' then (
+	  Buffer.add_char buf '\\';
+	  incr i
+        ) else if !i+2 < len &&
+	    is_first_oct_digit str.[!i] &&
+	    is_oct_digit str.[!i+1] &&
+	    is_oct_digit str.[!i+2] then (
+	      let byte = oct_val str.[!i] in
+	      incr i;
+	      let byte = (byte lsl 3) + oct_val str.[!i] in
+	      incr i;
+	      let byte = (byte lsl 3) + oct_val str.[!i] in
+	      incr i;
+	      Buffer.add_char buf (Char.of_int_exn byte)
+	    )
+      ) else (
+        incr i;
+        Buffer.add_char buf c
+      )
+    done;
+    Buffer.contents buf
+
+  (* PostgreSQL 9.0 introduced the new 'hex' format for binary data.
+     We must therefore check whether the data begins with a magic sequence
+     that identifies this new format and if so call the appropriate parser;
+     if it doesn't, then we invoke the parser for the old 'escape' format.
+  *)
+  let bytea_of_string str =
+    if String.is_prefix str ~prefix:"\\x"
+    then bytea_of_string_hex str
+    else bytea_of_string_escape str
+
+  let string_of_bytea b =
+    let len = String.length b in
+    let buf = Buffer.create (len * 2) in
+    for i = 0 to len - 1 do
+      let c = b.[i] in
+      let cc = Char.to_int c in
+      if  cc < 0x20 || cc > 0x7e || c = '\'' || c = '"' || c = '\\'
+      then
+        Buffer.add_string buf (sprintf "\\\\%03o" cc) (* non-print -> \ooo *)
+      else 
+        Buffer.add_char buf c (* printable *)
+    done;
+    Buffer.contents buf
+
+      
+  let to_db_input = string_of_bytea
+  let of_db_output = bytea_of_string
+
+end
+
+  
 module Sql_query = struct
   type t = string
   type result = string option list list
@@ -53,9 +148,10 @@ module Sql_query = struct
     v_sexp: Sexp.t;
   }
       
-  let escape_sql = 
-    String.Escaping.escape ~escapeworthy:['\''] ~escape_char:'\''
-    |! Staged.unstage
+  let escape_sql s = Bytea.to_db_input s
+  let unescape s =
+    (* eprintf "Unescaping: %S\n--> %S%!" s (Bytea.of_db_output s); *)
+    Bytea.of_db_output s
 
   let wipe_out: t list = [
     "drop table record;";
@@ -78,10 +174,10 @@ module Sql_query = struct
     sprintf
       "CREATE TABLE record (\n\
       \  id %s ,\n\
-      \  type text NOT NULL,\n\
-      \  created text NOT NULL,\n\
-      \  last_modified text NOT NULL,\n\
-      \  sexp text NOT NULL)" id_type
+      \  type bytea NOT NULL,\n\
+      \  created bytea NOT NULL,\n\
+      \  last_modified bytea NOT NULL,\n\
+      \  sexp bytea NOT NULL)" id_type
   let check_record: t =
     "SELECT (id, type, created, last_modified, sexp)\
      FROM record WHERE id = 0"
@@ -89,23 +185,23 @@ module Sql_query = struct
   let create_function: t =
     sprintf "CREATE TABLE function (\n\
       \  id %s,\n\
-      \  type text NOT NULL,\n\
+      \  type bytea NOT NULL,\n\
       \  result integer,\n\
       \  recomputable bool NOT NULL,\n\
       \  recompute_penalty real NOT NULL,\n\
-      \  inserted text NOT NULL,\n\
-      \  started text,\n\
-      \  completed text,\n\
-      \  status text NOT NULL,\n\
-      \  sexp text NOT NULL)" id_type
+      \  inserted bytea NOT NULL,\n\
+      \  started bytea,\n\
+      \  completed bytea,\n\
+      \  status bytea NOT NULL,\n\
+      \  sexp bytea NOT NULL)" id_type
   let check_function: t =
     "SELECT * FROM function WHERE id = 0"
 
   let create_volume: t =
     sprintf "CREATE TABLE volume (\n\
       \  id %s,\n\
-      \  kind text NOT NULL,\n\
-      \  sexp text NOT NULL)"  id_type
+      \  kind bytea NOT NULL,\n\
+      \  sexp bytea NOT NULL)"  id_type
   let check_volume: t =
     "SELECT (id, kind, sexp) FROM volume WHERE id = 0"
         
@@ -114,7 +210,7 @@ module Sql_query = struct
     let str_sexp = Sexp.to_string_hum sexp |! escape_sql in
     let str_type = escape_sql record_name in
     sprintf "INSERT INTO record (type, created, last_modified, sexp)\n\
-      \ VALUES ('%s', '%s', '%s', '%s') RETURNING id" str_type now now str_sexp
+      \ VALUES (E'%s', E'%s', E'%s', E'%s') RETURNING id" str_type now now str_sexp
 
   let add_evaluation_sexp
       ~function_name ~recomputable ~recompute_penalty ~status sexp : t =
@@ -140,7 +236,7 @@ module Sql_query = struct
     let last_modified = Timestamp.to_string v.r_last_modified |! escape_sql in
     let sexp          = Sexp.to_string v.r_sexp |! escape_sql in
     sprintf "INSERT INTO record (id, type, created, last_modified, sexp)\n\
-      \ VALUES (%d, '%s', '%s', '%s', '%s')"
+      \ VALUES (%d, '%s'::bytea, '%s'::bytea, '%s'::bytea, '%s'::bytea)"
       id typ created last_modified sexp
 
   let insert_evaluation v : t =
@@ -217,23 +313,30 @@ module Sql_query = struct
     | [one] -> Ok one
     | more -> Error (`result_not_unique more)
       
+  let parse_bool = function
+    | "f" | "false" | "False" | "FALSE" -> Ok false
+    | "t" | "true" | "True" | "TRUE" -> Ok true
+    | s -> Error (Failure (sprintf "parse_bool: %S" s))
+      
+  let parse_timestamp s =
+    Result.try_with (fun () -> Timestamp.of_string (unescape s))
+  let parse_timestamp_opt o =
+    Result.try_with (fun () ->
+      Option.map o ~f:(fun s -> Timestamp.of_string (unescape s)))
+
   let parse_value sol =
     match sol with
     | [ Some i; Some t; Some creat; Some last; Some sexp ] ->
       Result.(
         try_with (fun () -> Int.of_string i) >>= fun r_id ->
-        try_with (fun () -> Timestamp.of_string creat) >>= fun r_created ->
-        try_with (fun () -> Timestamp.of_string last) >>= fun r_last_modified ->
-        try_with (fun () -> Sexp.of_string sexp) >>= fun r_sexp ->
+        parse_timestamp creat >>= fun r_created ->
+        parse_timestamp last >>= fun r_last_modified ->
+        try_with (fun () -> Sexp.of_string (unescape sexp)) >>= fun r_sexp ->
         return {r_id; r_type = t; r_created; r_last_modified; r_sexp})
       |! Result.map_error ~f:(fun e -> `parse_value_error (sol, e))
     | _ -> Error (`parse_value_error (sol, Failure "Wrong format"))
 
   let parse_evaluation sol =
-    let parse_bool = function
-      | "f" | "false" | "False" | "FALSE" -> false
-      | "t" | "true" | "True" | "TRUE" -> true
-      | s -> failwithf "parse_bool: %S" s () in
     match sol with
     | [ Some id; Some typ; result_opt;
         Some recomputable; Some recompute_penalty;
@@ -242,18 +345,16 @@ module Sql_query = struct
       Result.(
         try_with (fun () -> Int.of_string id) >>= fun f_id ->
         return typ >>= fun f_type ->
-        try_with (fun () -> Option.map ~f:Int.of_string result_opt)
-                            >>= fun f_result ->
-        try_with (fun () -> parse_bool recomputable) >>= fun f_recomputable ->
-        try_with (fun () -> Float.of_string recompute_penalty)
-                            >>= fun f_recompute_penalty ->
-        try_with (fun () -> Timestamp.of_string inserted) >>= fun f_inserted ->
-        try_with (fun () -> Option.map ~f:Timestamp.of_string started_opt)
-                            >>= fun f_started ->
-        try_with (fun () -> Option.map ~f:Timestamp.of_string completed_opt)
-                            >>= fun f_completed ->
-        try_with (fun () -> status_of_string_exn status) >>= fun f_status ->
-        try_with (fun () -> Sexp.of_string sexp) >>= fun f_sexp ->
+        try_with (fun () ->
+          Option.map ~f:(fun s -> unescape s |! Int.of_string) result_opt
+        ) >>= fun f_result ->
+        parse_bool recomputable >>= fun f_recomputable ->
+        try_with (fun () -> Float.of_string recompute_penalty) >>= fun f_recompute_penalty ->
+        parse_timestamp inserted >>= fun f_inserted ->
+        parse_timestamp_opt started_opt >>= fun f_started ->
+        parse_timestamp_opt completed_opt >>= fun f_completed ->
+        try_with (fun () -> status_of_string_exn (unescape status)) >>= fun f_status ->
+        try_with (fun () -> Sexp.of_string (unescape sexp)) >>= fun f_sexp ->
         return {f_id; f_type; f_result; f_recomputable; f_recompute_penalty;
                 f_inserted; f_started; f_completed; f_status; f_sexp; })
       |! Result.map_error ~f:(fun e -> `parse_evaluation_error (sol, e))
@@ -264,7 +365,7 @@ module Sql_query = struct
     | [ Some i; Some k; Some sexp ] ->
       Result.(
         try_with (fun () -> Int.of_string i) >>= fun v_id ->
-        try_with (fun () -> Sexp.of_string sexp) >>= fun v_sexp ->
+        try_with (fun () -> Sexp.of_string (unescape sexp)) >>= fun v_sexp ->
         return {v_id; v_kind = k; v_sexp})
       |! Result.map_error ~f:(fun e -> `parse_value_error (sol, e))
     | _ -> Error (`parse_volume_error (sol, Failure "Wrong format"))
