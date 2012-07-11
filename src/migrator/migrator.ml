2ac6c6f356f4b38a6b794784a4e7a055d3cf26fd
Sebastien Mondet <seb@mondet.org>
2012-07-11 18:40:50 -0400

    Update the migrator

--- src/migrator/migrator.ml
+++ src/migrator/migrator.ml
@@ -12,20 +12,43 @@ let add_empty name =
       
 
 let v10_to_v11 file_in file_out =
+  let open Sexplib.Sexp in
   let dump_v10 =
     In_channel.(with_file file_in ~f:input_all) |! Sexplib.Sexp.of_string in
 
-  let () = V10.dump_of_sexp dump_v10 |! ignore in
+  let () = V10.Layout.dump_of_sexp dump_v10 |! ignore in
 
 
-  let dump_v11 = Atom "brout" in
-
+  let dump_v11 =
+    let rec parse = function
+      | Atom a -> Atom a
+      | List [Atom "version"; Atom old_version]
+          when old_version = V10.Info.version ->
+        eprintf "Changed version.\n"; List [Atom "version"; Atom V11.Info.version ]
+      | List [Atom "person"; List persons]  ->
+        eprintf "Persons...: %d\n" (List.length persons) ;
+        List [Atom "person";
+              List (List.map persons (function
+              | Atom a -> assert false
+              | List l -> 
+                List (List.map l ~f:(function
+                | List [Atom "g_value"; l] ->
+                  List [Atom "g_value"; (add_empty "affiliations" l)]
+                | l -> l))))]
+      | List l -> List (List.map l ~f:parse)
+    in
+    dump_v10
+    |! add_empty "hiseq_statistics"
+    |! add_empty "affiliation"
+    |! parse
+        
+  in
   
   let () =
     try
       V11.Layout.dump_of_sexp dump_v11 |! ignore
     with e ->
-      printf "Could not reparse in V11: %s" (Exn.to_string e)
+      eprintf "Could not reparse in V11: %s\n" (Exn.to_string e)
   in
   
   Out_channel.(with_file file_out ~f:(fun o ->
@@ -35,12 +58,7 @@ let v10_to_v11 file_in file_out =
     
 let () =
   match Array.to_list Sys.argv with
-  | exec :: "v08-v10" :: file_in :: file_out :: moves :: id_table :: [] ->
-    v08_to_v10 file_in file_out moves id_table
   | exec :: "v10-v11" :: file_in :: file_out :: [] ->
     v10_to_v11 file_in file_out
   | _ ->
-    eprintf "usage: %s {v08-v10} <dump-in> <dump-out> <moves-out> <id-table-out>\n"
-      Sys.argv.(0);
-    eprintf "usage: %s {v10-v11} <dump-in> <dump-out>\n"
-      Sys.argv.(0)
+    eprintf "usage: %s {v10-v11} <dump-in> <dump-out>\n" Sys.argv.(0)
