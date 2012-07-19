150ad8f93225e8ac7a362d198454cfe7b4c11573
Sebastien Mondet <seb@mondet.org>
2012-07-19 15:07:29 -0400

    Update the migrator

--- src/migrator/migrator.ml
+++ src/migrator/migrator.ml
@@ -56,9 +56,42 @@ let v10_to_v11 file_in file_out =
   ()
 
     
+let v11_to_v12 file_in file_out =
+  let open Sexplib.Sexp in
+  let dump_v11 =
+    In_channel.(with_file file_in ~f:input_all) |! Sexplib.Sexp.of_string in
+
+  let () = V11.Layout.dump_of_sexp dump_v11 |! ignore in
+
+
+  let dump_v12 =
+    let rec parse = function
+      | Atom a -> Atom a
+      | List [Atom "version"; Atom old_version]
+          when old_version = V11.Info.version ->
+        eprintf "Changed version.\n"; List [Atom "version"; Atom V12.Info.version ]
+      | List l -> List (List.map l ~f:parse)
+    in
+    dump_v11
+    |! parse
+        
+  in
+  
+  let () =
+    try
+      V12.Layout.dump_of_sexp dump_v12 |! ignore
+    with e ->
+      eprintf "Could not reparse in V12: %s\n" (Exn.to_string e)
+  in
+  
+  Out_channel.(with_file file_out ~f:(fun o ->
+    output_string o (Sexplib.Sexp.to_string_hum dump_v12)));
+  ()
 let () =
   match Array.to_list Sys.argv with
   | exec :: "v10-v11" :: file_in :: file_out :: [] ->
     v10_to_v11 file_in file_out
+  | exec :: "v11-v12" :: file_in :: file_out :: [] ->
+    v11_to_v12 file_in file_out
   | _ ->
-    eprintf "usage: %s {v10-v11} <dump-in> <dump-out>\n" Sys.argv.(0)
+    eprintf "usage: %s {v10-v11,v11-v12} <dump-in> <dump-out>\n" Sys.argv.(0)
