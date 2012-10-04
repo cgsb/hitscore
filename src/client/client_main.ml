cfd0dc700ad8eb093d76d026e239d4c12339ca32
Sebastien Mondet <seb@mondet.org>
2012-10-04 11:28:52 -0400

    client: use Sequme_flow_app_util

--- src/client/client_main.ml
+++ src/client/client_main.ml
@@ -3,12 +3,34 @@
 open Hitscore
 open Core.Std
 open Sequme_flow
+open Sequme_flow_app_util
+
+
+    
 
 let main () =
+  dbg "Starting: %s" Time.(now () |! to_string) >>= fun () ->
   return ()
 
+
+let info_command =
+  let open Command_line in
+  basic ~summary:"Get information"
+    Spec.(
+      anon (sequence "ARGS" string)
+    )
+    (fun args ->
+      run_flow ~on_error:(function
+      | `io_exn e ->
+        eprintf "End with ERRORS: %s" Exn.(to_string e))
+        begin
+          dbg "command not implemented"
+        end)
+
+    
 let () =
-  match Lwt_main.run (main ()) with
-  | Ok () -> ()
-  | Error e ->
-    eprintf "END WITH ERROR:\n%!"
+  Command_line.(
+    run ~version:"0"
+      (group ~summary:"Gencore's command-line application" [
+        ("info", info_command);
+      ]))
