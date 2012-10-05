fbb009eef4f423617bff61ffa1e5512f63eb87f1
Sebastien Mondet <seb@mondet.org>
2012-10-05 14:27:24 -0400

    Communication: add some documentation

--- src/lib/hitscore_communication.mli
+++ src/lib/hitscore_communication.mli
@@ -1,19 +1,27 @@
+(** Client/Server communication utilities. *)
 
+(** The definition of the messages and their serialization. *)
 module Protocol :
 sig
 
+  (** Messages from the client to the server. *)
   type up = [
-  | `log of string
+  | `log of string (** Ask the server to keep something in the logs. *)
   ]
 
+  (** Messages from the server to the client. *)
   type down = [
-  | `user_message of string
+  | `user_message of string (** Ask the client to display a message to
+                                the user *)
   ]
 
 end
 
+(** Utility functions related to authentication with passwords and
+    “tokens”. *)
 module Authentication : sig
 
+  (** Hash a password with a [Person.t] id using the “official” function. *)
   val hash_password : int -> string -> string
 
 end
