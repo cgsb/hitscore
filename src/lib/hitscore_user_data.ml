fd99d33ac59a8959cce40000b4ed476c804570f4
Sebastien Mondet <seb@mondet.org>
2013-01-04 12:12:45 -0500

    User_date: add function add_upload

--- src/lib/hitscore_user_data.ml
+++ src/lib/hitscore_user_data.ml
@@ -1,4 +1,5 @@
 open Hitscore_std
+module Layout = Hitscore_layout
   
 type t = {
   mutable uploads: String.Set.t;
@@ -11,5 +12,23 @@ let of_string s =
   with e -> Error (`sexp_parsing_error e)
 
     
+let add_upload ~dbh ~person_id filename =
+  let layout = Layout.Classy.make dbh in
+  begin
+    layout#person#get_unsafe person_id >>= fun person ->
+    begin match person#user_data with
+    | Some ud ->
+      of_result (of_string ud)
+    | None -> return { uploads = String.Set.empty }
+    end
+    >>= fun user_data ->
+  (* Adding a duplicate is considered OK, we could use String.Set.mem. *)
+    user_data.uploads <- String.Set.add user_data.uploads filename;
+    person#set_user_data (Some (to_string user_data))
+  end
+  >>< begin function
+  | Ok () -> return ()
+  | Error e -> error (`user_data ("add_upload", e))
+  end
   
  
--- src/lib/hitscore_user_data.mli
+++ src/lib/hitscore_user_data.mli
@@ -1,7 +1,19 @@
 open Hitscore_std
+
   
 type t
 
 val to_string: t -> string
 val of_string: string -> (t, [> `sexp_parsing_error of exn]) Result.t
 
+(** Register an “uploaded” file: [add_upload ~dbh ~person_id:42 "filename.pdf"]
+*)
+val add_upload :
+  dbh:Hitscore_db_backend.Backend.db_handle ->
+  person_id:int ->
+  string ->
+  (unit,
+   [> `user_data of
+       string *
+         [> `Layout of Hitscore_layout.Layout.error_location * Hitscore_layout.Layout.error_cause
+         | `sexp_parsing_error of exn ] ]) Hitscore_std.t
