51f7ebb70e8713fc35e8ab071a352b0b76cfab43
Sebastien Mondet <seb@mondet.org>
2013-04-12 11:44:15 -0400

    Conf_values: add an MLI file

---
+++ src/lib/hitscore_conf_values.mli
@@ -0,0 +1,8 @@
+(** Values about the Hitscore versions and state *)
+
+val version: string
+(** The current version *)
+
+
+val build_date: string
+(** The date ["hitscore_conf_values.ml"] was generated. *)
