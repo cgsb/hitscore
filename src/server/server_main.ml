43f0c3a776087e3959be4ac8169826cd61f0cfeb
Sebastien Mondet <seb@mondet.org>
2013-05-03 15:11:39 -0400

    server: return only delivered FASTQ files
    The is a point raised in #36.

--- src/server/server_main.ml
+++ src/server/server_main.ml
@@ -206,20 +206,24 @@ let list_libraries ~state query =
                           ~f:(fun d-> d#directory) del#client_fastqs_dir) in
               return path)
           >>= fun delivery_path ->
-          map_option dmux#unaligned (fun un ->
-            let files =
-              Data_access.user_file_paths ~unaligned:un ~submission:sub lib in
-            let prefix =
-              Option.value ~default:""
-                (Configuration.root_path state.global.configuration) in
-            let r1s =
-              List.map files#fastq_r1s (String.chop_prefix ~prefix)
-              |> List.filter_opt in
-            let r2s =
-              List.map files#fastq_r2s (String.chop_prefix ~prefix)
-              |> List.filter_opt in
-            return (r1s, r2s, read_number)
-            ))
+          begin match delivery_path with
+          | None -> return None
+          | Some del_path ->
+            map_option dmux#unaligned (fun un ->
+                let files =
+                  Data_access.user_file_paths ~unaligned:un ~submission:sub lib in
+                let prefix =
+                  Option.value ~default:""
+                    (Configuration.root_path state.global.configuration) in
+                let r1s =
+                  List.map files#fastq_r1s (String.chop_prefix ~prefix)
+                  |> List.filter_opt in
+                let r2s =
+                  List.map files#fastq_r2s (String.chop_prefix ~prefix)
+                  |> List.filter_opt in
+                return (r1s, r2s, read_number)
+              )
+          end)
         >>| List.filter_opt))
     >>| List.concat
     >>| List.concat
