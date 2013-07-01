20eee6c41d8056fa2bd34adc8323133a62dc6529
Sebastien Mondet <seb@mondet.org>
2013-07-01 11:52:01 -0400

    migrator: update with PGM and Barcode changes
    This is related to the issues #72 and #77.

--- src/migrator/migrator.ml
+++ src/migrator/migrator.ml
@@ -27,55 +27,7 @@ let map_g_values list_of_values ~f =
     | List [Atom "g_value"; l] -> List [Atom "g_value"; (f l)]
     | List [Atom "g_evaluation"; l] -> List [Atom "g_evaluation"; (f l)]
     | l -> l))))
-  (*
-let v13_to_v14 file_in file_out =
-  let open Sexplib.Sexp in
-  let dump_v13 =
-    In_channel.(with_file file_in ~f:input_all) |! Sexplib.Sexp.of_string in
 
-  let () = V13.Layout.dump_of_sexp dump_v13 |! ignore in
-
-  let dump_v14 =
-    let now = Time.(now () |! to_string) in
-    let parse = function
-      | Atom a -> Atom a
-      | List [Atom "version"; Atom old_version]
-          when old_version = V13.Info.version ->
-        eprintf "Changed version.\n"; List [Atom "version"; Atom V14.Info.version ]
-      | List [Atom "person"; List l ] ->
-        List [Atom "person"; List (map_g_values l (fun v ->
-          add_empty "auth_tokens" v |! add_empty "user_data"))]
-      | List [Atom "file_system"; List l ] ->
-        let f = function
-          | Atom s -> failwithf "atom : %s" s ()
-          | List l  ->
-            List ((List [Atom "g_last_modified"; Atom now]) :: l)
-        in
-        List [Atom "file_system"; List (List.map l ~f)]
-      | List l -> List l
-    in
-    begin match dump_v13 with
-    | Atom  a -> assert false
-    | List l ->
-      let sorted = List.sort l ~cmp:(fun a b ->
-        match a,b with
-        | List [Atom va; _], List [Atom vb; _] -> - (compare va vb)
-        | _ -> assert false) in
-      List (List.map sorted ~f:parse) |! add_empty "authentication_token"
-    end
-  in
-
-  let () =
-    try
-      V14.Layout.dump_of_sexp dump_v14 |! ignore
-    with e ->
-      eprintf "Could not reparse in V14: %s\n" (Exn.to_string e)
-  in
-
-  Out_channel.(with_file file_out ~f:(fun o ->
-    output_string o (Sexplib.Sexp.to_string_hum dump_v14)));
-  ()
-*)
 let v14_to_v15 file_in file_out =
   let open Sexplib.Sexp in
   let dump_v14 =
@@ -124,6 +76,83 @@ let v14_to_v15 file_in file_out =
     output_string o (Sexplib.Sexp.to_string_hum dump_v15)));
   ()
 
+let v15_to_v16 file_in file_out =
+  let open Sexplib.Sexp in
+  let dump_v15 =
+    In_channel.(with_file file_in ~f:input_all) |! Sexplib.Sexp.of_string in
+
+  let () = V15.Layout.dump_of_sexp dump_v15 |! ignore in
+  let dump_v16 =
+    let parse = function
+      | Atom a -> Atom a
+      | List [Atom "version"; Atom old_version]
+          when old_version = V15.Info.version ->
+        eprintf "Changed version.\n"; List [Atom "version"; Atom V16.Info.version ]
+      | List [Atom "barcode"; List barcodes]  ->
+        eprintf "barcodes: %d\n" (List.length barcodes) ;
+        let new_barcodes =
+          let module M15 = struct
+            type t = {
+              kind: string;
+              index: int option;
+              position_in_r1: int option;
+              position_in_r2: int option;
+              position_in_index: int option;
+              sequence: string option } with sexp
+          end in
+          let module M16 = struct
+            type t = {
+              kind: string;
+              index: int option;
+              read: int option;
+              position: int option;
+              sequence: string option } with sexp
+            let of_15 v =
+              let {M15. kind; index; position_in_r1; position_in_r2;
+                    position_in_index; sequence } = v in
+              let read, position =
+                match position_in_r1, position_in_r2, position_in_index with
+                | Some n, None, None -> Some 1, Some n
+                | None, Some n, None -> Some 3, Some n
+                | None, None, Some n -> Some 2, Some n
+                | None, None, None-> None, None
+                | _, _, _ -> failwithf "unexpected barcode record"  ()
+              in
+              { kind; index; read; position; sequence }
+          end in
+          map_g_values barcodes (fun g_value ->
+                let v15 = M15.t_of_sexp g_value in
+                (* eprintf "Got %s\n%!" v15.M15.kind; *)
+                M16.(sexp_of_t (of_15 v15)))
+        in
+        List [Atom "barcode"; List new_barcodes]
+      | List l -> List l
+    in
+    begin match dump_v15 with
+    | Atom  a -> assert false
+    | List l ->
+      let sorted = List.sort l ~cmp:(fun a b ->
+        match a,b with
+        | List [Atom va; _], List [Atom vb; _] -> - (compare va vb)
+        | _ -> assert false) in
+      List (List.map sorted ~f:parse)
+      |> add_empty "pgm_stock_library"
+      |> add_empty "pgm_input_library"
+      |> add_empty "pgm_pool"
+      |> add_empty "pgm_run"
+    end
+  in
+
+  let () =
+    try
+      V16.Layout.dump_of_sexp dump_v16 |! ignore
+    with e ->
+      eprintf "Could not reparse in V16: %s\n" (Exn.to_string e)
+  in
+
+  Out_channel.(with_file file_out ~f:(fun o ->
+    output_string o (Sexplib.Sexp.to_string_hum dump_v16)));
+  ()
 
 let () =
   match Array.to_list Sys.argv with
@@ -131,5 +160,7 @@ let () =
     v13_to_v14 file_in file_out*)
   | exec :: "v14-v15" :: file_in :: file_out :: [] ->
     v14_to_v15 file_in file_out
+  | exec :: "v15-v16" :: file_in :: file_out :: [] ->
+    v15_to_v16 file_in file_out
   | _ ->
-    eprintf "usage: %s {v14-v15} <dump-in> <dump-out>\n" Sys.argv.(0)
+    eprintf "usage: %s {v14-v15,v15-v16} <dump-in> <dump-out>\n" Sys.argv.(0)
