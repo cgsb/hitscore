438fb92982c5d33cd87004a39782c287eecff355
Sebastien Mondet <seb@mondet.org>
2014-01-08 18:21:30 -0500

    Std: add functions `flow_detach` and `flow_yield`

--- src/lib/hitscore_std.ml
+++ src/lib/hitscore_std.ml
@@ -17,3 +17,14 @@ let canonical_path s =
   match Filename.parts s |! List.reduce ~f:Filename.concat with
   | Some s -> s
   | None -> failwithf "FATAL : (canonical_path %S) returned None!" s ()
+
+let dbg fmt = ksprintf (fun s -> eprintf "HS-DBG: %s\n%!" s) fmt
+
+let dbgt fmt =
+  ksprintf (fun s -> dbg "%.4f: %s" Time.(now ()|> to_float) s) fmt
+
+let flow_detach f =
+  Lwt_preemptive.detach (fun () -> Ok (f ())) ()
+let flow_yield () =
+  wrap_io (fun () -> Lwt_main.yield ()) ()
+  
